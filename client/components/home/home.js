c32264d4936f294fafddcdd8a316784904b99ff1
miiwn <miiwnpmawt@gmail.com>
2018-03-19 13:51:51 +0700

    [EDIT] homepage button margin

--- client/components/home/home.js
+++ client/components/home/home.js
@@ -7,5 +7,5 @@ import { Button } from 'semantic-ui-react'
 export default () => (
   <div>
     <Sidebar /><h1 style={{'textAlign':"center"}}>Timetable Scheduling Processes</h1><Timetable/>
-    <Button primary style={{'float':'right','margin-right':'62px'}} >Create a timetable</Button></div>
+    <Button primary style={{'float':'right','marginRight':'62px'}} >Create a timetable</Button></div>
 )
