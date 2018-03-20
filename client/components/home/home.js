e7ad39d055e6fbd4d402446b3e8520827804787a
miiwn <miiwnpmawt@gmail.com>
2018-03-20 22:08:43 +0700

    [EDIT]home page strutured

--- client/components/home/home.js
+++ client/components/home/home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Sidebar from '../sidebar'
 import Timetable from '../timetable'
+import CreateModal from '../createmodal'
 import { Button } from 'semantic-ui-react'
 
 export default () => (
   <div>
+<<<<<<< HEAD
     <Sidebar />
     <h1 style={{ textAlign: 'center' }}>Timetable Scheduling Processes</h1>
     <Timetable />
@@ -12,4 +14,9 @@ export default () => (
       Create a timetable
     </Button>
   </div>
+=======
+    <Sidebar /><h1 style={{'textAlign':"center"}}>Timetable Scheduling Processes</h1>
+    <Timetable/>
+    <CreateModal/></div>
+>>>>>>> feature/modal
 )
