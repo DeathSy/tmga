bfbf8ef9a6a8d31a36b73a33421a0a154c23b3f0
miiwn <miiwnpmawt@gmail.com>
2018-03-20 21:42:11 +0700

    [EDIT]add modal to homepage

--- client/components/home/home.js
+++ client/components/home/home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Sidebar from '../sidebar'
 import Timetable from '../timetable'
+import CreateModal from '../createmodal'
 import { Button } from 'semantic-ui-react'
 
 
 export default () => (
   <div>
-    <Sidebar /><h1 style={{'textAlign':"center"}}>Timetable Scheduling Processes</h1><Timetable/>
-    <Button primary style={{'float':'right','marginRight':'62px'}} >Create a timetable</Button></div>
+    <Sidebar /><h1 style={{'textAlign':"center"}}>Timetable Scheduling Processes</h1>
+    <Timetable/>
+    <CreateModal/></div>
 )
