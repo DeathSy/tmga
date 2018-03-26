9b2690ce74bf8d9652023908100222df923e0ec0
deathsy <kittipong.sri@mail.kmutt.ac.th>
2018-03-26 22:37:47 +0700

    [EDIT] lint

--- client/components/home/home.js
+++ client/components/home/home.js
@@ -2,11 +2,12 @@ import React from 'react'
 import Sidebar from '../sidebar'
 import Timetable from '../timetable'
 import CreateModal from '../createmodal'
-import { Button } from 'semantic-ui-react'
 
 export default () => (
   <div>
-    <Sidebar /><h1 style={{'textAlign':"center"}}>Timetable Scheduling Processes</h1>
-    <Timetable/>
-    <CreateModal/></div>
+    <Sidebar />
+    <h1 style={{ textAlign: 'center' }}>Timetable Scheduling Processes</h1>
+    <Timetable />
+    <CreateModal />
+  </div>
 )
