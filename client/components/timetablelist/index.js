61697cf7c2ebf508a727a33603f6fa76edb9f4e0
miiwn <miiwnpmawt@gmail.com>
2018-03-28 15:34:30 +0700

    [ADD] timetablelist for timetable editor page

---
+++ client/components/timetablelist/index.js
@@ -0,0 +1,10 @@
+import TimetableList from './timetablelist'
+import { compose } from 'recompose'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+
+const mapStateToProps = state => ({})
+
+const mapDispatchToProps = dispatch => bindActionCreators({}, dispatch)
+
+export default compose(connect(mapStateToProps, mapDispatchToProps))(TimetableList)
