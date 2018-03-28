73919ac6ae818a4caf0cbe9b3a9cda1fbf7ff64f
miiwn <miiwnpmawt@gmail.com>
2018-03-28 15:33:43 +0700

    [EDIT] create modal structure

--- client/components/createmodal/createmodal.js
+++ client/components/createmodal/createmodal.js
@@ -11,23 +11,24 @@ export default class CreateModal extends Component {
 
         this.setState({ activeIndex: newIndex })
     }
-    
+
     render() {
-        const { activeIndex } = this.state 
+        const { activeIndex } = this.state
         const options = [
             { key: 1, text: '1/2561', value: 1 },
             { key: 2, text: '2/2561', value: 2 },
             { key: 3, text: '1/2562', value: 3 },
-          ]
+        ]
 
 
         return (
+
             <Modal trigger={<Button primary style={{ 'float': 'right', 'marginRight': '62px' }}>Create a timetable</Button>} size='small' closeIcon>
                 <Modal.Header scrolling>Create a timetable</Modal.Header>
-                <Input size='mini' placeholder='Search...' style={{'float':'right','margin':'12px 20px'}}  icon={{ name: 'search', link: true }} />
+                <Input size='mini' placeholder='Search...' style={{ 'float': 'right', 'margin': '12px 20px' }} icon={{ name: 'search', link: true }} />
                 <Modal.Content>
-                    <p> Semester: <Dropdown inline  defaultValue={options[0].value} options={options} style={{'width':'auto'}}/></p>
-                    <Accordion styled style={{'width':'auto'}}>
+                    <p> Semester: <Dropdown inline defaultValue={options[0].value} options={options} style={{ 'width': 'auto' }} /></p>
+                    <Accordion styled style={{ 'width': 'auto' }}>
                         <Accordion.Title active={activeIndex === 0} index={0} onClick={this.handleClick}>
                             <Checkbox onclick={this.handleClick} label={<label>INT101 IT Fundamental</label>} style={{ 'verticalAlign': 'middle', 'padding': '10px 16px' }} />
                             <Icon name='dropdown' float='right' />
@@ -47,8 +48,12 @@ export default class CreateModal extends Component {
 
                         </Accordion.Content>
                     </Accordion>
-                    <Button primary style={{ 'float': 'right', 'margin': '20px 0px' }}>Create a timetable</Button>
                 </Modal.Content>
+                <Modal.Actions>
+                <Button primary style={{ 'float': 'right', 'margin': '20px 0px' }}>
+                Create a timetable
+                </Button>
+                </Modal.Actions>
             </Modal>
         )
     }
