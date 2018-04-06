d6c2c0cff579cccaf9a31a98479cd92ea681c4e7
miiwn <miiwnpmawt@gmail.com>
2018-04-06 21:43:17 +0700

    [EDIT] separate style into file

--- client/components/timetablelist/timetablelist.js
+++ client/components/timetablelist/timetablelist.js
@@ -1,37 +1,41 @@
 import React from 'react'
 import { Table, Icon } from 'semantic-ui-react'
+import { Topic, TableHead, TableHeadCell, TableCellStyle } from './styles'
 
 const TimetableList = () => (
-  <Table
-    basic='very'
-    fixed
-    style={{ width: '750px', overflow: 'scroll', margin: '50px' }}
-  >
-    <Table.Header style={{ 'font-size': '20px' }}>
-      <Table.Row>
-        <Table.HeaderCell width={12} style={{ 'padding-left': '20px' }}>
-          Semester
-        </Table.HeaderCell>
-        <Table.HeaderCell style={{ 'padding-left': '20px' }}>
-          Status
-        </Table.HeaderCell>
-      </Table.Row>
-    </Table.Header>
-    <Table.Body>
-      <Table.Row>
-        <Table.Cell style={{ 'padding-left': '25px' }}>2/2560</Table.Cell>
-        <Table.Cell style={{ 'padding-left': '25px' }}>
-          {' '}<Icon size='big' color='black' name='search' />
-        </Table.Cell>
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell style={{ 'padding-left': '25px' }}>2/2561</Table.Cell>
-        <Table.Cell style={{ 'padding-left': '25px' }}>
-          {' '}<Icon size='big' color='black ' name='search' />
-        </Table.Cell>
-      </Table.Row>
-    </Table.Body>
-  </Table>
+  <div style={{ width: '780px', margin: '20px 300px', paddingTop: '30px' }}>
+    <Topic as='h1'>Timetable Editor</Topic>
+    <Table
+      basic='very'
+      fixed
+      style={{ width: '750px', overflow: 'scroll', margin: '50px' }}
+    >
+      <TableHead>
+        <Table.Row>
+          <TableHeadCell width={12}>
+            Semester
+          </TableHeadCell>
+          <TableHeadCell>
+            Status
+          </TableHeadCell>
+        </Table.Row>
+      </TableHead>
+      <Table.Body>
+        <Table.Row>
+          <TableCellStyle>2/2560</TableCellStyle>
+          <TableCellStyle>
+            {' '}<Icon size='big' color='black' name='search' />
+          </TableCellStyle>
+        </Table.Row>
+        <Table.Row>
+          <TableCellStyle>2/2561</TableCellStyle>
+          <TableCellStyle>
+            {' '}<Icon size='big' color='black ' name='search' />
+          </TableCellStyle>
+        </Table.Row>
+      </Table.Body>
+    </Table>
+  </div>
 )
 
 export default TimetableList
