642ee481c4dc46176799365eb4304dd3096c4343
dedathsy <kittipong.sri@mail.kmutt.ac.th>
2018-04-20 02:51:05 +0700

    [ADD] relations to lecturer model

--- api/models/lecturer.js
+++ api/models/lecturer.js
@@ -5,6 +5,10 @@ export const lecturerSchema = mongoose.Schema({
     type: String,
     required: true
   },
+  teachSubjects: [{
+    type: mongoose.Schema.ObjectId,
+    ref: 'Sections'
+  }],
   createdAt: String,
   updatedAt: String
 })
