b0daf97d5fb5e94a7438727b758f789bbb030785
dedathsy <kittipong.sri@mail.kmutt.ac.th>
2018-04-25 18:08:50 +0700

    [DEL] morgan

--- ml/app.js
+++ ml/app.js
@@ -1,5 +1,4 @@
 import express from 'express'
-import morgan from 'morgan'
 import { json, urlencoded } from 'body-parser'
 import cors from 'cors'
 
@@ -10,10 +9,6 @@ const app = express()
 app.use(cors())
 app.use(urlencoded({ extended: true }))
 app.use(json())
-
-if (process.env.NODE_ENV !== 'test') {
-  app.use(morgan('combined'))
-}
 
 app.use('/api/v1', routes)
 
