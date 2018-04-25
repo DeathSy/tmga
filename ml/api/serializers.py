32ab76171dcd700c995cb847d20b0dd2afd6f1eb
dedathsy <kittipong.sri@mail.kmutt.ac.th>
2018-04-25 15:50:01 +0700

    [DEL] python ml

--- ml/api/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Bucketlist
-
-
-class BucketlistSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Bucketlist
-        fields = ('id', 'name', 'createdAt', 'updatedAt')
-        read_only_fields = ('createdAt', 'updatedAt')
