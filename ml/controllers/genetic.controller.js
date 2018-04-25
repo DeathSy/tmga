1873b2f25146e7d58e542a9e495e298777fd44f9
dedathsy <kittipong.sri@mail.kmutt.ac.th>
2018-04-25 18:29:18 +0700

    [ADD] genetic controller

--- ml/controllers/genetic.controller.js
+++ ml/controllers/genetic.controller.js
@@ -1,3 +1,13 @@
-export const startGenerateSchedual = async (req, res) => {
-  res.status(200).send({ message: 'Hello' })
+import { exec } from 'child-process-promise'
+
+export const startGenerateSchedual = (req, res) => {
+  exec('babel-node models/genetic.js')
+    .then(({ stdout }) => {
+      console.log(stdout)
+    })
+    .catch(({ stderr }) => {
+      console.log(stderr)
+    })
+
+  res.send({ message: 'Timetable is start generating' })
 }
