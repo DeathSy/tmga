87ff40918ee2a94ff141e4af67c579dbe3d75531
miiwn <miiwnpmawt@gmail.com>
2018-05-01 00:25:45 +0700

    [EDIT] timetable layout

--- client/components/timetable/timetable.js
+++ client/components/timetable/timetable.js
@@ -3,7 +3,7 @@ import { Table, Icon } from 'semantic-ui-react'
 import { Topic, TableHead, TableHeadCell, TableCellStyle } from './styles'
 
 const TimetableList = () => (
-  <div style={{ width: '780px', margin: '20px 300px', paddingTop: '30px' }}>
+  <div style={{ width: '1100px', margin: '20px 360px', paddingTop: '30px' }}>
     <Topic as='h1'>Timetable Scheduling Processes</Topic>
     <Table
       basic='very'
