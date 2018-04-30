50a5ba9fca22cfadb83941810ff2f93c3d3e0f1b
miiwn <miiwnpmawt@gmail.com>
2018-05-01 00:27:32 +0700

    [ADD] editlecturer page

---
+++ client/components/lecturers/editlecturer/editlecturer.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import { Button, Checkbox, Form, Input, Icon } from 'semantic-ui-react'
+
+class AddLecturers extends Component {
+  state = {}
+
+  handleChange = (e, { value }) => this.setState({ value })
+
+  render () {
+
+    const { value } = this.state
+    return (
+      <div style={{ width: '1100px', margin: '20px 360px', paddingTop: '30px' }}>
+        <h1> <Icon name='left chevron' />Edit - Aj.Atchara TranUraikul</h1>
+        <Form size='large' style={{ width: '1000px', marginLeft: '30px', padding: '30px' }}>
+          <Form.Field inline control={Input} label='First name:' placeholder='First name' value={'Atchara'} readOnly style={{ width: '400px' }}/>
+          <Form.Field inline control={Input} label='Last name:' placeholder='Last name' value={'TranUraikul'} readOnly style={{ width: '400px' }}/>
+          <Form.Field inline control={Input} label='Email:' placeholder='Email' value={'atchara@sit.kmutt.ac.th'}style={{ width: '400px' }} />
+          <Form.Field inline control={Input} label='Tel:' placeholder='Tel' value={'021234567'}style={{ width: '400px' }}/>
+          <Form.Group inline>
+            <Form.Field inline control={Input} label='Subject:' placeholder='Subject Code' style={{ width: '200px' }} />
+            <Icon name='plus' />
+          </Form.Group>
+          <Button primary floated='right' size='large'>Edit</Button>
+          <Button floated='right' size='large'>Cancel</Button>
+        </Form>
+      </div>
+    )
+  }
+}
+
+export default AddLecturers
