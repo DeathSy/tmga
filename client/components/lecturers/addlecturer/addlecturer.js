ddc9ba173bdff05a2b85b43282e7ba76e1daf50a
miiwn <miiwnpmawt@gmail.com>
2018-05-01 00:27:00 +0700

    [EDIT] lecturer management layout

--- client/components/lecturers/addlecturer/addlecturer.js
+++ client/components/lecturers/addlecturer/addlecturer.js
@@ -10,19 +10,19 @@ class AddLecturers extends Component {
 
     const { value } = this.state
     return (
-      <div style={{ marginLeft: '300px', marginTop: '30px' }}>
-        <h1 style={{ margin: '30px 0px' }}> <Icon name='left chevron' />Create New Lecturer</h1>
-        <Form style={{ width: '700px', marginLeft: '30px' }}>
-          <Form.Field inline control={Input} label='First name:' placeholder='First name' style={{ width: '250px' }}/>
-          <Form.Field inline control={Input} label='Last name:' placeholder='Last name' style={{ width: '250px' }}/>
-          <Form.Field inline control={Input} label='Email:' placeholder='Email' style={{ width: '250px' }} />
-          <Form.Field inline control={Input} label='Tel:' placeholder='Tel' style={{ width: '250px' }}/>
+      <div style={{ width: '1100px', margin: '20px 360px', paddingTop: '30px' }}>
+        <h1> <Icon name='left chevron' />Create New Lecturer</h1>
+        <Form size='large' style={{ width: '1000px', marginLeft: '30px', padding: '30px' }}>
+          <Form.Field inline control={Input} label='First name:' placeholder='First name' style={{ width: '400px' }}/>
+          <Form.Field inline control={Input} label='Last name:' placeholder='Last name' style={{ width: '400px' }}/>
+          <Form.Field inline control={Input} label='Email:' placeholder='Email' style={{ width: '400px' }} />
+          <Form.Field inline control={Input} label='Tel:' placeholder='Tel' style={{ width: '400px' }}/>
           <Form.Group inline>
-            <Form.Field inline control={Input} label='Subject:' placeholder='Subject Code' style={{ width: '150px' }} />
+            <Form.Field inline control={Input} label='Subject:' placeholder='Subject Code' style={{ width: '200px' }} />
             <Icon name='plus' />
           </Form.Group>
-          <Button primary floated='right'>Add</Button>
-          <Button floated='right'>Cancel</Button>
+          <Button primary floated='right' size='large'>Edit</Button>
+          <Button floated='right' size='large'>Cancel</Button>
         </Form>
       </div>
     )
--- client/components/lecturers/lecturerdetail/lecturerdetail.js
+++ client/components/lecturers/lecturerdetail/lecturerdetail.js
@@ -2,30 +2,29 @@ import React, { Component } from 'react'
 import { Card, Grid, Button, Icon } from 'semantic-ui-react'
 
 const LecturerDetail = () => (
-  <div style={{ marginLeft: '270px', marginTop: '30px' }}>
-    <h1 style={{ margin: '30px 0px' }}> <Icon name='left chevron' />Lecturers</h1>
+  <div style={{ width: '1100px', margin: '20px 360px', paddingTop: '30px' }}>
+    <h1> <Icon name='left chevron' />Lecturers</h1>
     <h2 style={{ marginLeft: '35px' }}>Aj.Atchara TranUraikul</h2>
-    <Grid divided='vertically'>
+    <Grid size='large' divided='vertically'>
       <Grid.Row columns={2}>
         <Grid.Column>
-          <h4 style={{ marginLeft: '100px' }}>Name:Aj.Atchara TranUraikul </h4>
-          <h4 style={{ marginLeft: '100px' }}>Email:      atchara@sit.kmutt.ac.th</h4>
-          <h4 style={{ marginLeft: '100px' }}>Tel:      024709866</h4>
-          <h4 style={{ marginLeft: '100px' }}>Subject </h4>
-          <Card style={{ height: '100px', marginLeft: '100px' }} >
+          <h3 style={{ margin: '30px 100px' }}>Name:Aj.Atchara TranUraikul </h3>
+          <h3 style={{ margin: '30px 100px' }}>Email:      atchara@sit.kmutt.ac.th</h3>
+          <h3 style={{ margin: '30px 100px' }}>Tel:      024709866</h3>
+          <h3 style={{ margin: '30px 100px' }}>Subject </h3>
+          <Card style={{ height: '200px', width: '340px', marginLeft: '100px' }} >
             <Card.Content>
               INT102
             </Card.Content>
           </Card>
         </Grid.Column>
-
       </Grid.Row>
     </Grid>
-    <Button primary floated='right' style={{ marginTop: '30px', marginRight: '100px' }}>
+    <Button primary size='large' floated='right' style={{ marginTop: '30px', marginRight: '90px' }}>
       Edit
       <Icon name='right chevron' />
     </Button>
-    <Button floated='right' style={{ marginTop: '30px' }}>Delete</Button>
+    <Button size='large'floated='right' style={{ marginTop: '30px' }}>Delete</Button>
   </div>
 )
 
