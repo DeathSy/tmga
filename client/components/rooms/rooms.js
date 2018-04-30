52ee390594a33f9379d5d5b52aad3e4972dac718
miiwn <miiwnpmawt@gmail.com>
2018-05-01 00:35:58 +0700

    [EDIT] room lint error

--- client/components/rooms/rooms.js
+++ client/components/rooms/rooms.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Input, Icon, Card, Button, Item, Label, Accordion } from 'semantic-ui-react'
+import { Icon, Button, Item, Label, Accordion } from 'semantic-ui-react'
 import { Topic } from './styles'
 
 export default class Rooms extends Component {
@@ -13,101 +13,96 @@ export default class Rooms extends Component {
     this.setState({ activeIndex: newIndex })
   }
 
-  render() {
+  render () {
     const { activeIndex } = this.state
 
     return (
       <div style={{ width: '1100px', margin: '20px 360px', paddingTop: '30px' }}>
         <Topic as='h1'>Data Management</Topic>
-      <Topic as='h2'>Rooms</Topic>
-
+        <Topic as='h2'>Rooms</Topic>
         <Accordion fluid styled>
-        <Accordion.Title active={activeIndex === 0} index={0} onClick={this.handleClick}>
-          <Icon name='dropdown' />
-          Classroom Building II
-          
-        </Accordion.Title>
-        <Accordion.Content active={activeIndex === 0}>
-        <Item.Group divided>
-          <Item>
-            <Item.Content>
-              <Item.Header as='a'>CB2301</Item.Header>
-              <Item.Extra>
-                <Button primary floated='right' size='tiny'>
-                  Edit
-                  <Icon name='right chevron' />
-                </Button>
-                <Button floated='right' size='tiny'>
-                  Delete
-                </Button>
-                <Label><Icon name='users' /> 100</Label>
-                <Label><Icon name='write' />Lecture</Label>
-              </Item.Extra>
-            </Item.Content>
-          </Item>
-          <Item>
-            <Item.Content>
-              <Item.Header as='a'>CB2304</Item.Header>
-              <Item.Extra>
-                <Button primary floated='right' size='tiny'>
-                  Edit
-                  <Icon name='right chevron' />
-                </Button>
-                <Button floated='right' size='tiny'>
-                  Delete
-                </Button>
-                <Label><Icon name='users' /> 35 </Label>
-                <Label><Icon name='write' />Lecture</Label>
-
-              </Item.Extra>
-            </Item.Content>
-          </Item>
-          </Item.Group>
-        </Accordion.Content>
-
-        <Accordion.Title active={activeIndex === 1} index={1} onClick={this.handleClick}>
-          <Icon name='dropdown' />
-          SIT Building
-        </Accordion.Title>
-        <Accordion.Content active={activeIndex === 1}>
-        <Item.Group divided>
-          <Item>
-            <Item.Content>
-              <Item.Header as='a'>Training Room I</Item.Header>
-              <Item.Extra>
-                <Button primary floated='right' size='tiny'>
-                  Edit
-                  <Icon name='right chevron' />
-                </Button>
-                <Button floated='right' size='tiny'>
-                  Delete
-                </Button>
-                <Label><Icon name='users' />35 </Label>
-                <Label><Icon name='lab' />Lab</Label>
-              </Item.Extra>
-            </Item.Content>
-          </Item>
-          <Item>
-            <Item.Content>
-              <Item.Header as='a'>Training Room II</Item.Header>
-              <Item.Extra>
-                <Button primary floated='right' size='tiny'>
-                  Edit
-                  <Icon name='right chevron' />
-                </Button>
-                <Button floated='right' size='tiny'>
-                  Delete
-                </Button>
-                <Label><Icon name='users' />35 </Label>
-                <Label><Icon name='lab' />Lab</Label>
-              </Item.Extra>
-            </Item.Content>
-          </Item>
-          </Item.Group>
-        </Accordion.Content>
+          <Accordion.Title active={activeIndex === 0} index={0} onClick={this.handleClick}>
+            <Icon name='dropdown' />
+            Classroom Building II
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 0}>
+            <Item.Group divided>
+              <Item>
+                <Item.Content>
+                  <Item.Header as='a'>CB2301</Item.Header>
+                  <Item.Extra>
+                    <Button primary floated='right' size='tiny'>
+                      Edit
+                      <Icon name='right chevron' />
+                    </Button>
+                    <Button floated='right' size='tiny'>
+                      Delete
+                    </Button>
+                    <Label><Icon name='users' /> 100</Label>
+                    <Label><Icon name='write' />Lecture</Label>
+                  </Item.Extra>
+                </Item.Content>
+              </Item>
+              <Item>
+                <Item.Content>
+                  <Item.Header as='a'>CB2304</Item.Header>
+                  <Item.Extra>
+                    <Button primary floated='right' size='tiny'>
+                      Edit
+                      <Icon name='right chevron' />
+                    </Button>
+                    <Button floated='right' size='tiny'>
+                      Delete
+                    </Button>
+                    <Label><Icon name='users' /> 35 </Label>
+                    <Label><Icon name='write' />Lecture</Label>
+                  </Item.Extra>
+                </Item.Content>
+              </Item>
+            </Item.Group>
+          </Accordion.Content>
+          <Accordion.Title active={activeIndex === 1} index={1} onClick={this.handleClick}>
+            <Icon name='dropdown' />
+            SIT Building
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 1}>
+            <Item.Group divided>
+              <Item>
+                <Item.Content>
+                  <Item.Header as='a'>Training Room I</Item.Header>
+                  <Item.Extra>
+                    <Button primary floated='right' size='tiny'>
+                      Edit
+                      <Icon name='right chevron' />
+                    </Button>
+                    <Button floated='right' size='tiny'>
+                      Delete
+                    </Button>
+                    <Label><Icon name='users' />35 </Label>
+                    <Label><Icon name='lab' />Lab</Label>
+                  </Item.Extra>
+                </Item.Content>
+              </Item>
+              <Item>
+                <Item.Content>
+                  <Item.Header as='a'>Training Room II</Item.Header>
+                  <Item.Extra>
+                    <Button primary floated='right' size='tiny'>
+                      Edit
+                      <Icon name='right chevron' />
+                    </Button>
+                    <Button floated='right' size='tiny'>
+                      Delete
+                    </Button>
+                    <Label><Icon name='users' />35 </Label>
+                    <Label><Icon name='lab' />Lab</Label>
+                  </Item.Extra>
+                </Item.Content>
+              </Item>
+            </Item.Group>
+          </Accordion.Content>
         </Accordion>
       </div>
     )
   }
 }
-)
