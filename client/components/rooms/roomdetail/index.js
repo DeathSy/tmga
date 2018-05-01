764d710cd51aa222a08dc093143a969d0b1287bc
miiwn <miiwnpmawt@gmail.com>
2018-05-01 16:08:10 +0700

    [ADD] roomdetail page

---
+++ client/components/rooms/roomdetail/index.js
@@ -0,0 +1,10 @@
+import RoomDetail from './roomdetail'
+import { compose } from 'recompose'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+
+const mapStateToProps = state => ({})
+
+const mapDispatchToProps = dispatch => bindActionCreators({}, dispatch)
+
+export default compose(connect(mapStateToProps, mapDispatchToProps))(RoomDetail)
