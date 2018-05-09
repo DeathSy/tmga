f6b909751d0c4f9a866bca17b3dbefb42d6c5066
miiwn <miiwnpmawt@gmail.com>
2018-05-09 15:50:18 +0700

    [EDIT] addlecturer component to grid

--- client/components/lecturers/addlecturer/addlecturer.js
+++ client/components/lecturers/addlecturer/addlecturer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Button, Checkbox, Form, Input, Icon } from 'semantic-ui-react'
+import { Button, Form, Input, Icon, Grid } from 'semantic-ui-react'
 
 class AddLecturers extends Component {
   state = {}
@@ -7,23 +7,35 @@ class AddLecturers extends Component {
   handleChange = (e, { value }) => this.setState({ value })
 
   render () {
-
-    const { value } = this.state
+    const subjectOptions = [
+      { key: 'int101', value: 'int101', text: 'INT101' },
+      { key: 'int102', value: 'int102', text: 'INT102' }]
     return (
-      <div style={{ width: '1100px', margin: '20px 360px', paddingTop: '30px' }}>
+      <div style={{ width: '1000px', margin: '20px 360px', paddingTop: '30px' }}>
         <h1> <Icon name='left chevron' />Create New Lecturer</h1>
-        <Form size='large' style={{ width: '1000px', marginLeft: '30px', padding: '30px' }}>
-          <Form.Field inline control={Input} label='First name:' placeholder='First name' style={{ width: '400px' }}/>
-          <Form.Field inline control={Input} label='Last name:' placeholder='Last name' style={{ width: '400px' }}/>
-          <Form.Field inline control={Input} label='Email:' placeholder='Email' style={{ width: '400px' }} />
-          <Form.Field inline control={Input} label='Tel:' placeholder='Tel' style={{ width: '400px' }}/>
-          <Form.Group inline>
-            <Form.Field inline control={Input} label='Subject:' placeholder='Subject Code' style={{ width: '200px' }} />
-            <Icon name='plus' />
-          </Form.Group>
-          <Button primary floated='right' size='large'>Add</Button>
-          <Button floated='right' size='large'>Cancel</Button>
-        </Form>
+        <Grid size='huge' divided='vertically'>
+          <Grid.Column width={5}>
+            <h3 style={{ margin: '30px 100px' }}>First Name:</h3>
+            <h3 style={{ margin: '30px 100px' }}>Last Name:</h3>
+            <h3 style={{ margin: '30px 100px' }}>Email: </h3>
+            <h3 style={{ margin: '30px 100px' }}>Tel: </h3>
+            <h3 style={{ margin: '30px 100px' }}>Subject:</h3>
+          </Grid.Column>
+          <Grid.Column floated='left' width={3}>
+            <Form style={{ marginTop: '25px' }}>
+              <Form.Field inline control={Input} placeholder='First name' style={{ width: '400px' }}/>
+              <Form.Field inline control={Input} placeholder='Last name' style={{ width: '400px' }}/>
+              <Form.Field inline control={Input} placeholder='Email' style={{ width: '400px' }} />
+              <Form.Field inline control={Input} placeholder='Tel' style={{ width: '400px' }}/>
+              <Input icon placeholder='Subject'>
+                <Icon name='add' />
+                <input />
+              </Input>
+            </Form>
+          </Grid.Column>
+        </Grid>
+        <Button primary floated='right' size='large'>Add</Button>
+        <Button floated='right' size='large'>Cancel</Button>
       </div>
     )
   }
