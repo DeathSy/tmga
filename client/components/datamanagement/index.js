d31f57e25eae07d96091f6b78ad73a3d0f08fdff
miiwn <miiwnpmawt@gmail.com>
2018-05-10 21:40:17 +0700

    [EDIT] folder managelecturermodal to datamanagement

---
+++ client/components/datamanagement/index.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import Lecturer from './lecturers'
+import Rooms from './rooms'
+import Subjects from './subjects'
+import { compose } from 'recompose'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+
+const mapStateToProps = state => ({})
+
+const mapDispatchToProps = dispatch => bindActionCreators({}, dispatch, dispatch)
+class Management extends Component {
+  render () {
+    return (
+      <div>
+        <Lecturer />
+        <Rooms />
+        <Subjects />
+      </div>
+    )
+  }
+}
+
+export default compose(
+  connect(mapStateToProps, mapDispatchToProps)
+)(Management)
---
+++ client/components/datamanagement/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+import { TableCell, Segment, TableHeaderCell, Header } from 'semantic-ui-react'
+
+export const Topic = styled(Header)`
+  margin: 500px;
+`
+
+export const SegmentStyle = styled(Segment)`
+  width: 400px;
+  height: 200px;
+`
+
+export const TableHeadCell = styled(TableHeaderCell)`
+  padding-left: 20px;
+`
+
+export const TableCellStyle = styled(TableCell)`
+  padding-left: 25px;
+`
