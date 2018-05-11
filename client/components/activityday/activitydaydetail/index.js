372bf77eff57ed6c729e86e3f48c67a5ec7db2aa
miiwn <miiwnpmawt@gmail.com>
2018-05-11 17:02:33 +0700

    [ADD] activityday detail cpn

---
+++ client/components/activityday/activitydaydetail/index.js
@@ -0,0 +1,10 @@
+import ActivityDayDetail from './activitydaydetail'
+import { compose } from 'recompose'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+
+const mapStateToProps = state => ({})
+
+const mapDispatchToProps = dispatch => bindActionCreators({}, dispatch)
+
+export default compose(connect(mapStateToProps, mapDispatchToProps))(ActivityDayDetail)
