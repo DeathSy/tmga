9ec868d2de0426428c7deabd1682c06f259db29e
miiwn <miiwnpmawt@gmail.com>
2018-05-13 19:33:44 +0700

    [EDIT] Lecturer add add-button

--- client/components/lecturers/lecturers.js
+++ client/components/lecturers/lecturers.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import { Input, Icon, Card, Button, Item, Label } from 'semantic-ui-react'
-import { Topic } from './styles'
+import { Input, Icon, Card, Button, Item, Label, Header } from 'semantic-ui-react'
 
 const Lecturers = () => (
   <div style={{ width: '1100px', margin: '20px 360px', paddingTop: '30px' }}>
-    <Topic as='h1'>Data Management</Topic>
-    <Topic as='h2'>Lecturers</Topic>
+    <Header as='h1'>Data Management</Header>
+    <Header as='h2'>Lecturers</Header>
     <Card fluid scrolling >
       <Card.Content>
+        <Button primary floated='right'><Icon name='plus' />Add</Button>
         <Input icon='search' placeholder='Search...' style={{ 'float': 'right' }} /><Item.Group divided>
           <Item>
             <Item.Content>
