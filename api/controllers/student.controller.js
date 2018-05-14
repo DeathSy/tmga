2386b551cafe7cd71374dff2c187c9cd3e204099
dedathsy <kittipong.sri@mail.kmutt.ac.th>
2018-05-14 07:24:36 +0700

    [ADD] student controller

--- api/controllers/student.controller.js
+++ api/controllers/student.controller.js
@@ -1,9 +1,51 @@
-import Student from '../models/student'
+import {
+  getAll,
+  get,
+  create,
+  update,
+  remove
+} from '../repositories/student.repo'
 
-export const getAllStudents = async (req, res) => {
+export const getAllStudent = async (req, res) => {
   try {
-    const { data } = await Student.find(req.quey)
-    res.status(200).send(data)
+    const result = await getAll(req.query)
+    res.status(200).send(result)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+}
+
+export const getStudent = async (req, res) => {
+  try {
+    const result = await get(req.id)
+    res.status(200).send(result)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+}
+
+export const createStudent = async (req, res) => {
+  try {
+    const result = await create(req.body)
+    res.status(200).send(result)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+}
+
+export const updateStudent = async (req, res) => {
+  try {
+    const result = await update(req.id, req.body)
+    res.status(200).send(result)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+}
+
+export const deleteStudent = async (req, res) => {
+  try {
+    const result = await remove(req.id)
+    res.status(200).send(result)
   } catch (err) {
     res.status(500).send(err)
   }
